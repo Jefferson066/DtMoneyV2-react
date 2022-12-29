2b23cf2611bf5523bb051e7c73f667a3429f3c59
Jefferson Santos <jeffersonscr12@gmail.com>
2022-12-29 11:04:28 -0300

    responsividade

--- src/componets/Summary/styles.ts
+++ src/componets/Summary/styles.ts
@@ -1,42 +1,49 @@
 import styled, { css } from "styled-components";
 
 export const SummaryContainer = styled.section`
-  width: 100%;
-  max-width: 1120px;
-  margin: 0 auto;
-  padding: 0 1.5rem;
+    width: 100%;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 0 1.5rem;
 
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 2rem;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
 
-  margin-top: -5rem;
+    margin-top: -5rem;
+
+    @media screen and (max-width: 900px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (max-width: 700px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 `;
 
 interface SummaryCardprops {
-  variant?: "green";
+    variant?: "green";
 }
 
 export const SummaryCard = styled.div<SummaryCardprops>`
-  background: ${(props) => props.theme["gray-600"]};
-  padding: 2rem;
-  border-radius: 6px;
+    background: ${(props) => props.theme["gray-600"]};
+    padding: 2rem;
+    border-radius: 6px;
 
-  header {
-    display: flex;
-    margin-top: 1rem;
-    font-size: 2rem;
-  }
+    header {
+        display: flex;
+        margin-top: 1rem;
+        font-size: 2rem;
+    }
 
-  strong {
-    display: block;
-    margin-top: 1rem;
-    font-size: 2rem;
-  }
+    strong {
+        display: block;
+        margin-top: 1rem;
+        font-size: 2rem;
+    }
 
-  ${(props) =>
-    props.variant === "green" &&
-    css`
-      background: ${(props) => props.theme["green-700"]};
-    `}
+    ${(props) =>
+        props.variant === "green" &&
+        css`
+            background: ${(props) => props.theme["green-700"]};
+        `}
 `;
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -5,6 +5,7 @@ export const TransactionsContainer = styled.main`
   max-width: 1120px;
   margin: 4rem auto 0;
   padding: 0 1.5rem;
+  overflow-x: auto;
 `;
 
 export const TransactionsTable = styled.table`
@@ -12,10 +13,11 @@ export const TransactionsTable = styled.table`
   border-collapse: separate;
   border-spacing: 0 0.5rem;
   margin-top: 1.5rem;
-
+ 
   td {
     padding: 1.25rem 2rem;
     background: ${(props) => props.theme["gray-700"]};
+    
 
     &:first-child {
       border-top-left-radius: 6px;
